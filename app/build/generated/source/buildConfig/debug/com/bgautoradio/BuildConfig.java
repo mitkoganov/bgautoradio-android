65cf533ff6bda702444ec6c58ff0e7985073e96e
mitkoganov <mitko@nadezhda.bg>
2026-05-19 10:31:58 +0300

    v1.0.1 — OTA + Permissions checker + App Launcher + Floating Rail

--- app/build/generated/source/buildConfig/debug/com/bgautoradio/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/bgautoradio/BuildConfig.java
@@ -7,8 +7,8 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.bgautoradio.debug";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "1.0.0-debug";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "1.0.1-debug";
   // Field from default config.
   public static final String HERE_API_KEY = "d9-Fu1_un99EwPAFJXnTbxDMXD8ZEbvOcXPZ7bETSQ4";
   // Field from default config.
